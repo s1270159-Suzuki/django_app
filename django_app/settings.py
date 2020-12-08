473b6214b0cbdc2c081007e8bfe477848975572c
Haruya Suzuki <nuanyelingmu@gmail.com>
2020-12-09 02:21:48 +0900

    change SECRET_KEY

--- django_app/settings.py
+++ django_app/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1fq45d%5&=z+-)wz-g$aro^)60j#hm(ge#g=!lfgy^f*e!g@1r'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
